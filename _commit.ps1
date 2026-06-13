44479c4c56af303262d449ed0db7f15d8015a11b
Daniel Fisseha <developedbydanielfisseha@gmail.com>
2026-06-13 13:03:48 +0300

    chore: gitignore temp files, remove reference txts and _tmp from tracking

--- _commit.ps1
+++ _commit.ps1
@@ -1,4 +1,4 @@
 Set-Location "C:\Users\ROG\Desktop\oz-account-policy-builder"
 git add -A
-git commit -m "chore: remove dead contract refs from workspace, contracts/ is empty"
+git commit -m "chore: gitignore temp files, remove reference txts and _tmp from tracking"
 git push origin main
